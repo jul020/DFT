4919d26b214d847b20caf11eba3c00f4abf3de7a
Junjie Li <jul018@ucsd.edu>
2020-05-15 00:38:28 -0700

    code

--- run1.py
+++ run1.py
@@ -2,37 +2,36 @@ import os
 import shutil
 
 # Load the Si.pw.in.template file as a template.
-with open("PbTiO3.pw.in.template") as f:
+with open("PbTiO3.Ti.pw.in.template") as f:
     template = f.read()
 
 # Set default values for various parameters
-k = 6
-alat = 7.5 # The lattice parameter for the cell in Bohr.
+k = 10
+alat = 720 # The lattice parameter for the cell in Bohr.
+calat = 1.06
 tx=0.5
-ty=0.5
+ty=0.548
 tz=0.5
 
 #for calat in range(173,175,1):
 #calat=calat/100
-for k in range(6,18,2):
+for alat in range(720,760,5):
        # This generates a string from the template with the parameters replaced
        # by the specified values.
 
-       # alat=alat/100
-       # ty=ty/1000
+       alat=alat/100
 
-       #s = template.format(alat=alat, calat=calat, k=k)
-       s = template.format(alat=alat, k=k, tx=tx, ty=ty, tz=tz)
+       s = template.format(alat=alat, calat=calat, k=k, tx=tx, ty=ty, tz=tz)
 
        # Let's define an easy jobname
-       jobname = "PTO_k_%s_%s" % (alat, k)
+       jobname = "PTO_Ti_%s_%s_%s" % (alat, calat, k)
 
        # Write the actual input file for PWSCF.
        with open("%s.pw.in" % jobname, "w") as f:
            f.write(s)
 
        #Print some status messages.
-       print("Running with alat = %s, k = %s..." % (alat, k))
+       print("Running with alat = %s, calat = %s..." % (alat, calat))
 
        # Run PWSCF. Modify the pw.x command accordingly if needed.
        os.system("pw.x -inp {jobname}.pw.in > {jobname}.out".format(jobname=jobname))
